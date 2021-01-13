64289dbc1a4a3be6d63b7cbe371b4a87b3f325a9
Naveen Kumar <456endson@gmail.com>
2021-01-13 14:43:51 +0530

    added stateful logic to render snackbar with appropriate content when needed

--- src/components/Pallete.js
+++ src/components/Pallete.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
 import Navbar from "./Navbar";
+import SnackBar from "./SnackBar";
 import generateScaledPallete from "../utilities/scaleColors";
 import classes from "../stylesheets/Pallete.module.css";
 
@@ -10,9 +11,13 @@ class Pallete extends Component {
     this.state = {
       copiedColor: "",
       scaleValue: 8,
+      isVisible: false,
+      snackbarText: "",
+      snackbarStatus: "failure",
     };
     this.setCopiedColor = this.setCopiedColor.bind(this);
     this.setScaleValue = this.setScaleValue.bind(this);
+    this.setSnackbarVisible = this.setSnackbarVisible.bind(this);
   }
 
   setCopiedColor(code) {
@@ -23,6 +28,18 @@ class Pallete extends Component {
 
   setScaleValue(value) {
     this.setState({ scaleValue: parseInt(value) });
+  }
+
+  setSnackbarVisible(content, status) {
+    this.setState(
+      { isVisible: true, snackbarText: content, snackbarStatus: status },
+      () => {
+        setTimeout(
+          () => this.setState({ isVisible: false, snackbarText: "" }),
+          3500
+        );
+      }
+    );
   }
 
   render() {
@@ -47,6 +64,7 @@ class Pallete extends Component {
               code={color.color}
               copiedClass={classes["show-msg"]}
               setCopiedColor={this.setCopiedColor}
+              showSnackbar={this.setSnackbarVisible}
               key={color.name}
             />
           ))}
@@ -56,6 +74,12 @@ class Pallete extends Component {
           <h1>Copied !</h1>
           <h3>{this.state.copiedColor}</h3>
         </div>
+
+        <SnackBar
+          content={this.state.snackbarText}
+          show={this.state.isVisible}
+          status={this.state.snackbarStatus}
+        />
       </div>
     );
   }
