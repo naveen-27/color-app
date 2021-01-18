19f42576f6f8a803682369f2980b6bfa3dd1e919
Naveen Kumar <456endson@gmail.com>
2021-01-18 10:47:58 +0530

    created routing for main palettes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
-import colorPaletts from "./utilities/seedColors";
-import Pallete from "./components/Pallete";
+import { Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+import colorPalettes from "./utilities/seedColors";
+import Palette from "./components/Pallete";
 
 class App extends Component {
+  generatePalette(routeParams) {
+    const paletteId = routeParams.match.params.id;
+    const foundPalette = colorPalettes.find(
+      (palette) => palette.id === paletteId
+    );
+    return <Palette palette={foundPalette} />;
+  }
+
   render() {
     return (
       <div className="App">
-        <Pallete palette={colorPaletts[1]} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/palette/:id" render={this.generatePalette} />
+        </Switch>
       </div>
     );
   }
