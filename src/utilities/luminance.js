05799d598868de8d8e4ed55d4a2e67a3244e61c3
Naveen Kumar <456endson@gmail.com>
2021-01-19 20:01:18 +0530

    utility function to decide text color based on contrast

---
+++ src/utilities/luminance.js
@@ -0,0 +1,17 @@
+import chroma from "chroma-js";
+
+function getContrastColor(bg) {
+  const textLuminance = chroma("#edf1f2").luminance();
+  const bgLuminance = chroma(bg).luminance();
+  const contrast = textLuminance / bgLuminance;
+
+  console.log(contrast);
+
+  if (contrast < 4) {
+    return chroma("#edf1f2").darken(5).hex();
+  } else {
+    return "#edf1f2";
+  }
+}
+
+export default getContrastColor;
