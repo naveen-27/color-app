f2f6619735643fdf0ed1fd55894f54bb2f08827f
Naveen Kumar <456endson@gmail.com>
2021-01-22 19:18:18 +0530

    added transition effects when color is deleted and added

--- src/components/DraggablePalette.js
+++ src/components/DraggablePalette.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { SortableContainer } from "react-sortable-hoc";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import DraggableColorBox from "./DraggableColorBox";
 import classes from "../stylesheets/DraggablePalette.module.css";
 
@@ -8,17 +9,19 @@ class DraggablePalette extends Component {
     const { palette } = this.props;
 
     return (
-      <div className={classes.Palette}>
+      <TransitionGroup className={classes.Palette}>
         {palette.map((color, idx) => (
-          <DraggableColorBox
-            index={idx}
-            name={color.name}
-            hex={color.color}
-            key={color.name}
-            delete={this.props.deleteColor}
-          />
+          <CSSTransition key={color.name} timeout={500} classNames="fade">
+            <DraggableColorBox
+              index={idx}
+              name={color.name}
+              hex={color.color}
+              key={color.name}
+              delete={this.props.deleteColor}
+            />
+          </CSSTransition>
         ))}
-      </div>
+      </TransitionGroup>
     );
   }
 }
